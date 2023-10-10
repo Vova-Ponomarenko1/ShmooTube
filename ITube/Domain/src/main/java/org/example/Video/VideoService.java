8ae547c1ab7c37b6b358966bf3bf50518561d509
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2023-10-10 10:58:59 +0300

    Removed duplicate code in the HeadSideController and VideoController.

--- ITube/Domain/src/main/java/org/example/Video/VideoService.java
+++ ITube/Domain/src/main/java/org/example/Video/VideoService.java
@@ -4,8 +4,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public interface VideoService {
     void saveVideo(String title, String description, MultipartFile file, MultipartFile photo) throws IOException;
+
+    List<Thumbnail> updateThumbnailsWithBase64DataUri();
+
+
 }
--- ITube/Domain/src/main/java/org/example/Video/VideoServiceImpl.java
+++ ITube/Domain/src/main/java/org/example/Video/VideoServiceImpl.java
@@ -1,14 +1,13 @@
 package org.example.Video;
 
-import org.example.Video.Video;
-import org.example.Video.VideoRepository;
-import org.example.Video.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
 
 @Service
 public class VideoServiceImpl implements VideoService {
@@ -21,7 +20,6 @@ public class VideoServiceImpl implements VideoService {
     @Override
     @Transactional
     public void saveVideo(String title, String description, MultipartFile file, MultipartFile photo) throws IOException {
-
         byte[] videoData = file.getBytes();
         byte[] photoData = photo.getBytes();
 
@@ -31,8 +29,17 @@ public class VideoServiceImpl implements VideoService {
         video.setVideoData(videoData);
         video.setThumbnail(photoData);
 
-        videoValidator.videoValidate(video); // МОЖЛИВІ БАГИ НЕ ЗАТЕСТИВ ТАКОЖ НЕ ЗАВЕРЕШНА
+        videoValidator.videoValidate(video);
 
         videoRepository.save(video);
+    }
+    public List<Thumbnail> updateThumbnailsWithBase64DataUri() {
+        List<Thumbnail> getAllThumbnailsWithId = videoRepository.getAllThumbnailsWithIds();
+        getAllThumbnailsWithId.parallelStream().forEach(thumbnail -> {
+            byte[] thumbnailBytes = thumbnail.getThumbnail();
+            String dataUri = "data:image/png;base64," + Base64.getEncoder().encodeToString(thumbnailBytes);
+            thumbnail.setDataUri(dataUri);
+        });
+        return getAllThumbnailsWithId;
     }
 }

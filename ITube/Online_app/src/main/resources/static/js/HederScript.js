63139ab5c97a7f0766923e9b8a3b674aaa924914
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2023-10-10 11:06:08 +0300

    A request for a correct search operation has been added.

--- ITube/Online_app/src/main/resources/static/js/HederScript.js
+++ ITube/Online_app/src/main/resources/static/js/HederScript.js
@@ -9,3 +9,31 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+ //Search
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const searchInput = form.querySelector('input[type="text"]');
+        const searchText = searchInput.value;
+        const url = '/ITube/search';
+        const data = new FormData();
+        data.append('searchText', searchText);
+        fetch(url, {
+            method: 'POST',
+            body: data
+         })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                 }
+                window.location.href = '/ITube/searchResults?searchText=' + searchText;
+            })
+            .catch(error => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    });
+});
+

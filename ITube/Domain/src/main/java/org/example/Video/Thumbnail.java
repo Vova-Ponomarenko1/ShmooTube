34509fe471f61e75dbab958fe7a0724c20c7b882
Vova-Ponomarenko1 <116120076+Vova-Ponomarenko1@users.noreply.github.com>
2023-10-10 11:00:33 +0300

    Added new options

--- ITube/Domain/src/main/java/org/example/Video/Thumbnail.java
+++ ITube/Domain/src/main/java/org/example/Video/Thumbnail.java
@@ -2,11 +2,31 @@ package org.example.Video;
 
 public class Thumbnail {
     private long id;
-    private byte[] thumbnail;//
+    private byte[] thumbnail;
 
     private String dataUri;
 
     private String title;
+
+    private String userAvatarBase64;
+
+    private Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserAvatarBase64() {
+        return userAvatarBase64;
+    }
+
+    public void setUserAvatarBase64(String userAvatarBase64) {
+        this.userAvatarBase64 = userAvatarBase64;
+    }
 
     public String getTitle() {
         return title;
